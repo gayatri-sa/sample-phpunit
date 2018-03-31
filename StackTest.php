88c73f2832a0dfad349e884ad00a3b37db954442
GayatriM <gayatri@schogini.com>
2018-03-31 09:48:15 +0530

    Introduce an error

--- StackTest.php
+++ StackTest.php
@@ -5,7 +5,7 @@ class StackTest extends TestCase
 {
     public function testPushAndPop()
     {
-        $stack = [];
+        $stack = ['Hello'];
         $this->assertEquals(0, count($stack));
 
         array_push($stack, 'foo');
